d1963dc5cab4f0a336f3f16ff86d9afedc23919b
Yurui-Feng <yurifeng@bu.edu>
2024-11-28 21:14:45 -0800

    Add Abstract Logic

--- winter_supp_rules_engine/engine.py
+++ winter_supp_rules_engine/engine.py
@@ -1,10 +1,30 @@
-from winter_supp_rules_engine import rule
+# from rule import Rule
+
+class Rule:
+    def __init__(self, condition, action):
+        self.condition = condition
+        self.action = action
+    
 
 class winter_supplement_rule_engine():
     def __init__(self, *rules):
-        self.rules = rules
+        self.rules = set(rules)
+    
+    def add_rule(self, rule):
+        self.rules.add(rule)
     
     def run(self,state):
         for rule in self.rules:
             if rule.condition(state):
                 return rule.action(state)
+
+Engine = winter_supplement_rule_engine()
+
+#Eligible
+Engine.add_rule(Rule({"familyUnitInPayForDecember==False"}, {"isEligible":False, "baseAmount":0, "supplementAmount":0})) #short-circuit 
+#Single
+Engine.add_rule(Rule({"familyUnitInPayForDecember==True", "familyComposition==single", "dependent=int:n"}, {"isEligible":True, "baseAmount":60,"supplementAmount":20}))
+#Couple
+Engine.add_rule(Rule({"familyUnitInPayForDecember==True", "familyComposition==couple", "dependent=int:n"}, {"isEligible":True, "baseAmount":120,"supplementAmount":20}))
+
+print(Engine.rules)
