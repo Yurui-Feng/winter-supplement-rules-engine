ac53ecb7f91ba52f4cc5ae0e2170f3b727d47f48
Yurui-Feng <yurifeng@bu.edu>
2024-11-29 23:13:29 -0800

    Refactor: separating rules and engine definitions

--- rules_engine/engine.py
+++ rules_engine/engine.py
@@ -1,6 +1,5 @@
-# from rule import Rule
-from enum import Enum
 from typing import Callable
+from enum import Enum
 
 #priority functions similiar to a short circuit logic
 #e.g. Egilibility has high priority where the rest have low priority
@@ -9,41 +8,14 @@ class Priority(Enum):
     Medium = 2
     Low = 1
 
-def get_priority_value(item):
-    return item.priority.value
-
-def condition_eligibility(state:dict):
-    return state["familyUnitInPayForDecember"]
-
-def action_eligibility(state:dict):
-    return 60
-
-def condition_children(state:dict):
-    children_count = state["numberOfChildren"]
-    return children_count > 0
-
-def action_children_count(state:dict):
-    children_count = state["numberOfChildren"]
-    return 120 + 20 * children_count
-
-def condition_couple_family(state:dict):
-    if state["familyComposition"] == "couple":
-        return True
-    else:
-        return False
-
-def action_couple_family(state:dict):
-    return 120
-
 class Rule:
     def __init__(self, condition: Callable, action: Callable, priority:Priority):
         self.condition = condition
         self.action = action
         self.priority = priority
 
-Rule_Eligiblity = Rule(condition_eligibility, action_eligibility, Priority.High)
-Rule_Child_Count = Rule(condition_children, action_children_count, Priority.Medium)
-Rule_Couple = Rule(condition_couple_family, action_couple_family, Priority.Low)
+def get_priority_value(item):
+    return item.priority.value
         
 class winter_supplement_rule_engine():
     def __init__(self, *rules):
@@ -66,11 +38,3 @@ class winter_supplement_rule_engine():
                 supplement_amount = rule.action(state)
         
         return supplement_amount
-
-Engine = winter_supplement_rule_engine()
-Engine.add_rule(Rule_Eligiblity)
-Engine.add_rule(Rule_Child_Count)
-Engine.add_rule(Rule_Couple)
-
-state = {"familyUnitInPayForDecember":True, "familyComposition":"couple", "numberOfChildren":2}
-print(Engine.run(state))
---
+++ rules_engine/rules.py
@@ -0,0 +1,29 @@
+#This file defines the business rules
+from engine import Rule, Priority
+
+def condition_eligibility(state:dict):
+    return state["familyUnitInPayForDecember"]
+
+def action_eligibility(state:dict):
+    return 60
+
+def condition_children(state:dict):
+    children_count = state["numberOfChildren"]
+    return children_count > 0
+
+def action_children_count(state:dict):
+    children_count = state["numberOfChildren"]
+    return 120 + 20 * children_count
+
+def condition_couple_family(state:dict):
+    if state["familyComposition"] == "couple":
+        return True
+    else:
+        return False
+
+def action_couple_family(state:dict):
+    return 120
+
+Rule_Eligiblity = Rule(condition_eligibility, action_eligibility, Priority.High)
+Rule_Child_Count = Rule(condition_children, action_children_count, Priority.Medium)
+Rule_Couple = Rule(condition_couple_family, action_couple_family, Priority.Low)
